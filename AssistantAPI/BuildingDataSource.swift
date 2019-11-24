630964726f9adb11ce5c63f4e9bedd7c28c38ffe
Tolunay Arslan <tolunayarslancontact@gmail.com>
2019-11-24 11:31:44 +0100

    Created the BuildingDataSource Struct.

---
+++ AssistantAPI/BuildingDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  BuildingDataSource.swift
+//  AssistantAPI
+//
+//  Created by Arslan, Tolunay on 24.11.19.
+//  Copyright © 2019 Arslan, Tolunay. All rights reserved.
+//
+
+import Foundation
+
+struct BuildingDataSource {
+    
+}
